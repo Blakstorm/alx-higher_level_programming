0c93a4132665a8b26b13db0d1d694e4354977d0f
Blakstorm <riz.essaib@gmail.com>
2023-10-02 16:53:10 -0700

    0. Simple rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,16 +3,16 @@
 Defines a class Rectangle
 """
 
- a class Rectangle
+a class Rectangle
  """
 
 
  class Rectangle:
      """Representation of a rectangle"""
-         def __init__(self, width=0, height=0):
+     def __init__(self, width=0, height=0):
                  """Initializes the rectangle"""
-                         self.height = height
-                                 self.width = width
+                 self.height = height
+                 self.width = width
 
                                      @property
                                          def width(self):
